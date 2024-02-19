15bced18fe0a7f03c372ce25e6a0f11615c95109
Ander Gonzalez <ander.gonzalez@ikerlan.es>
2024-02-19 07:27:43 +0000

    Apply ament_clang_format to source

--- jsk_rviz_plugins/src/image_transport_hints_property.cpp
+++ jsk_rviz_plugins/src/image_transport_hints_property.cpp
@@ -3,13 +3,14 @@
 
 #include "image_transport_hints_property.hpp"
 
-namespace jsk_rviz_plugins {
+namespace jsk_rviz_plugins
+{
 
-ImageTransportHintsProperty::ImageTransportHintsProperty(const char* name,
-                                                         const char* description,
-                                                         rviz_common::properties::Property* parent,
-                                                         const char* changed_slot)
-    : rviz_common::properties::EditableEnumProperty(name, "raw", description, parent, changed_slot) {
+ImageTransportHintsProperty::ImageTransportHintsProperty(
+  const char * name, const char * description, rviz_common::properties::Property * parent,
+  const char * changed_slot)
+: rviz_common::properties::EditableEnumProperty(name, "raw", description, parent, changed_slot)
+{
   addOptionStd("raw");
   addOptionStd("compressed");
   addOptionStd("theora");
@@ -18,8 +19,10 @@ ImageTransportHintsProperty::ImageTransportHintsProperty(const char* name,
 
 ImageTransportHintsProperty::~ImageTransportHintsProperty() {}
 
-image_transport::TransportHints ImageTransportHintsProperty::getTransportHints(const rclcpp::Node * node) {
+image_transport::TransportHints ImageTransportHintsProperty::getTransportHints(
+  const rclcpp::Node * node)
+{
   return image_transport::TransportHints(node, getStdString());
 }
 
-}
+}  // namespace jsk_rviz_plugins
--- jsk_rviz_plugins/src/image_transport_hints_property.hpp
+++ jsk_rviz_plugins/src/image_transport_hints_property.hpp
@@ -1,24 +1,26 @@
 // ROS 2 Port:
 // Ander González Tomé <ander.gonzalez@ikerlan.es> @ IKERLAN S. COOP.
 
-#ifndef JSK_RVIZ_PLUGINS_IMAGE_TRANSPORT_HINTS_PROPERTY_HPP
-#define JSK_RVIZ_PLUGINS_IMAGE_TRANSPORT_HINTS_PROPERTY_HPP
+#ifndef JSK_RVIZ_PLUGINS_IMAGE_TRANSPORT_HINTS_PROPERTY_HPP_
+#define JSK_RVIZ_PLUGINS_IMAGE_TRANSPORT_HINTS_PROPERTY_HPP_
 
-#include <rviz_common/properties/property.hpp>
-#include <rviz_common/properties/editable_enum_property.hpp>
 #include <image_transport/transport_hints.hpp>
+#include <rviz_common/properties/editable_enum_property.hpp>
+#include <rviz_common/properties/property.hpp>
 
-namespace jsk_rviz_plugins {
+namespace jsk_rviz_plugins
+{
 
 class ImageTransportHintsProperty : public rviz_common::properties::EditableEnumProperty
 {
   Q_OBJECT
- public:
-  ImageTransportHintsProperty(const char* name, const char* description,
-                              rviz_common::properties::Property* parent, const char* changed_slot);
+public:
+  ImageTransportHintsProperty(
+    const char * name, const char * description, rviz_common::properties::Property * parent,
+    const char * changed_slot);
   ~ImageTransportHintsProperty();
 
   image_transport::TransportHints getTransportHints(const rclcpp::Node * node);
 };
-}
-#endif
+}  // namespace jsk_rviz_plugins
+#endif  // JSK_RVIZ_PLUGINS_IMAGE_TRANSPORT_HINTS_PROPERTY_HPP_
